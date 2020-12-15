daa15b13b2b261f55c9111fb2fa3c05f0b6f5027
Bobby Fitz <thecreativefitz@gmail.com>
2020-12-15 01:00:48 -0500

    Finished lessons 21, 22, and 23 of Section 2

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,21 +5,33 @@ int main()
     //Print welcome messages to the terminal
     std::cout << "You have discovered a bomb in the middle of a bank";
     std::cout << std::endl;
-    std::cout << "If you wish to save everyone you are going to have to enter the correct disarm code";
+    std::cout << "If you wish to save everyone you are going to have to enter the correct disarm code" << std::endl;
 
     // Declare 3 number code
-    const int a = 4;
-    const int b = 5;
-    const int c = 6;
+    const int CodeA = 4;
+    const int CodeB = 5;
+    const int CodeC = 6;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
     //Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
     
+    int GuessA, GuessB, GuessC;
+
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
+
+    std::cout << "The sum of your guesses is: "<< GuessSum << std::endl;
+    std::cout << "The product of your guesses is: " << GuessProduct << std::endl;
 
     return 0;
 }
