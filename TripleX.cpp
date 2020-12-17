aaea14bcfd352992fd217998049f91300aa5b3fd
Bobby Fitz <thecreativefitz@gmail.com>
2020-12-17 00:56:53 -0500

    Finished up to Lesson 27 of Section 2

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-int main()
+// This function prints the welcome message
+void IntroductionMessage(){
+    std::cout << "\n\nYou have discovered a bomb in the middle of a bank\n\n";
+    std::cout << "If you wish to save everyone you are going to have to enter the correct disarm code\n\n";
+}
+
+bool PlayGame()
 {
-    //Print welcome messages to the terminal
-    std::cout << "You have discovered a bomb in the middle of a bank";
-    std::cout << std::endl;
-    std::cout << "If you wish to save everyone you are going to have to enter the correct disarm code" << std::endl;
+//Print welcome messages to the terminal
+    IntroductionMessage();
 
     // Declare 3 number code
     const int CodeA = 4;
@@ -17,21 +21,43 @@ int main()
 
     //Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
-    
-    int GuessA, GuessB, GuessC;
+    std::cout << "+ There are 3 numbers in the code\n";
+    std::cout << "\n+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cout << "\nPlease enter the code before it explodes!!!\n\n";
+    
+    // Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     const int GuessSum = GuessA + GuessB + GuessC;
     const int GuessProduct = GuessA * GuessB * GuessC;
 
     std::cout << "The sum of your guesses is: "<< GuessSum << std::endl;
-    std::cout << "The product of your guesses is: " << GuessProduct << std::endl;
+    std::cout << "\nThe product of your guesses is: " << GuessProduct << std::endl;
+
+
+    // Check if the players guess matches the code in order
+    if (GuessA == 4 && GuessB == 5 && GuessC == 6){
+        std::cout<<"\nYou win!";
+        return true;
+    }
+    else{
+        std::cout<<"You lose, game over!!!";
+        return false;
+    }
+}
+
+
+int main()
+{
+    while(true)
+    {
+        bool bLevelComplete = PlayGame();
+        std::cin.clear(); //Clears any errors
+        std::cin.ignore(); //Discards the buffer
+    };
 
     return 0;
 }
